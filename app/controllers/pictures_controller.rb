efaddbd30624cda469e12baaa25b0a1c8d19a3b4
Brad Wilcox <brad_wilcox@me.com>
2013-07-20 11:49:36 -0400

    Refactoring to save new pictures in database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,53 +2,26 @@ class PicturesController < ApplicationController
 
 	def index
 		@pictures = Picture.all
-		 # @pictures = [
-   #    {
-   #      :title  => "The old church on the coast of White sea",
-   #      :artist => "Sergey Ershov",
-   #      :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-   #    },
-   #    {
-   #      :title  => "Sea Power",
-   #      :artist => "Stephen Scullion",
-   #      :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-   #    },
-   #    {
-   #      :title  => "Into the Poppies",
-   #      :artist => "John Wilhelm",
-   #      :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-   #    }
-   #  ]
 	end
 
 	def show
 		@picture = Picture.find(params[:id])
-
-	  # @pictures = [
-	  #   {
-	  #     :title  => "The old church on the coast of White sea",
-	  #     :artist => "Sergey Ershov",
-	  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-	  #   },
-	  #   {
-	  #     :title  => "Sea Power",
-	  #     :artist => "Stephen Scullion",
-	  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-	  #   },
-	  #   {
-	  #     :title  => "Into the Poppies",
-	  #     :artist => "John Wilhelm",
-	  #     :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-	  #   }
-	  # ]
-	  # @picture = @pictures[params[:id].to_i]
 	end
 
 	def new
+		@picture = Picture.new
 	end
 
 	def create
-		 render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-	end
+		# Make the variable @picture a new picture with the given params
+	  @picture = Picture.new(params[:picture])
+		  if @picture.save
+		  	# if the save for the picture was successful, go to index.html.erb
+		    redirect_to pictures_url
+		  else
+		  	# otherwise render the view associated with the action :new (i.e. new.html.erb)
+		    render :new
+		  end
+		end
 
 end
